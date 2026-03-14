68adf43ed4e2415e294f0d7951984bda30d2bea6
Emmanuel Alvarado <emmanuel_alvap@hotmail.com>
2026-03-13 23:59:24 -0600

    init

--- src/app/debt/debt.ts
+++ src/app/debt/debt.ts
@@ -1,7 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit, signal } from '@angular/core';
 import { DebtService, DebtResponse } from '../services/debt/debt-service';
-//import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 
@@ -13,11 +12,13 @@ import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 })
 export class Debt {
 
-    debtValues: DebtResponse[] = [];
+    sarimaxValues = signal<DebtResponse[]>([]);
+    vecmValues = signal<DebtResponse[]>([]);
 
     constructor(private http: HttpClient){}
 
-    apiUrl = 'http://localhost:8089/api/debt/predictions/';
+    apiUrlSarimax = 'http://localhost:8089/api/debt/predictions/sarimax/';
+    apiUrlVecm = 'http://localhost:8089/api/debt/predictions/vecm/';
 
     countries = [
         { code: 'MEX', name: 'Mexico' },
@@ -28,37 +29,83 @@ export class Debt {
     selectedCountry: string | null = null;
     loading = signal(false);
     errorMessage = signal("");
+    currencyMessage = signal("");
+
+    isSarimaxChecked = false;
+    isVecmChecked = false;
+
+    onCheckboxSarimaxChange(event: any) {
+        //console.log('Checkbox sarimax value:', event.target.checked);
+    }
+    onCheckboxVecmChange(event: any) {
+        //console.log('Checkbox vecm value:', event.target.checked);
+    }
 
     fetchData() {
 
+        this.sarimaxValues.set([]);
+        this.vecmValues.set([]);
+
         console.log("En el fetch data");
         const start = performance.now();
-    
-    
       
         this.errorMessage.update(value => "");
-        this.loading.update(value => true);
+        
 
         if (!this.selectedCountry) {
             this.errorMessage.update(value => "Selecciona un Pais");
             return;
         }
+        if(!this.isSarimaxChecked && !this.isVecmChecked){
+            this.errorMessage.update(value => "Selecciona un modelo");
+            return;
+        }
+        this.loading.update(value => true);
 
-        let apiUrlTemp = this.apiUrl + this.selectedCountry;
-        console.log("API URL: " + apiUrlTemp);
-        this.http.get<DebtResponse[]>(apiUrlTemp).subscribe({
-            next: (data) => {
-                this.debtValues = data;
-                this.loading.update(value => false);
-                this.errorMessage.update(value => "");
-                const end = performance.now();
-                console.log(`Execution time: ${(end - start).toFixed(2)} ms`);
-            },
-            error: (err) => {
-                this.errorMessage.update(value => "Error al cargar datos. Intenta mas tarde");
-                console.error(err);
-                this.loading.update(value => false);
-            }
-        });
+        if(this.isSarimaxChecked){
+
+            let apiUrlTemp = this.apiUrlSarimax + this.selectedCountry;
+            console.log("API URL: " + apiUrlTemp);
+            this.http.get<DebtResponse[]>(apiUrlTemp).subscribe({
+                next: (data) => {
+                    this.sarimaxValues.set([...data]);
+                    this.loading.update(value => false);
+                    this.errorMessage.update(value => "");
+                    this.currencyMessage.update(value => "Valores expresados en USD")
+                    const end = performance.now();
+                    console.log(`Sarimax Execution time: ${(end - start).toFixed(2)} ms`);
+                },
+                error: (err) => {
+                    this.errorMessage.update(value => "Sarimax - Error al cargar datos. Intenta mas tarde");
+                    this.currencyMessage.update(value => "");
+                    console.error(err);
+                    this.loading.update(value => false);
+                }
+            });
+        }
+        
+
+        if(this.isVecmChecked){
+
+            let apiUrlTemp2 = this.apiUrlVecm + this.selectedCountry;
+            console.log("API URL: " + apiUrlTemp2);
+            this.http.get<DebtResponse[]>(apiUrlTemp2).subscribe({
+                next: (data) => {
+                    this.vecmValues.set([...data]);
+                    this.loading.update(value => false);
+                    this.errorMessage.update(value => "");
+                    this.currencyMessage.update(value => "Valores expresados en USD")
+                    const end = performance.now();
+                    console.log(`Vecm Execution time: ${(end - start).toFixed(2)} ms`);
+                },
+                error: (err) => {
+                    this.errorMessage.update(value => "Vecm - Error al cargar datos. Intenta mas tarde");
+                    this.currencyMessage.update(value => "");
+                    console.error(err);
+                    this.loading.update(value => false);
+                }
+            });
+        }
+        
     }
 }
